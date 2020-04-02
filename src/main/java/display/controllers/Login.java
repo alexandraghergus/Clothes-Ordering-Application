47a71b58601456a975d88d389ec6e3567def60cc
alexandraghergus <andreea.ghergus@student.upt.ro>
2020-04-02 18:03:44 +0300

    Added gradle

---
+++ src/main/java/display/controllers/Login.java
@@ -0,0 +1,21 @@
+package display.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Login {
+    @FXML
+    public Text loginMessage;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public TextField usernameField;
+
+    @FXML
+    public void handleLogin(){
+        // should handle the login button
+
+    }
+}
